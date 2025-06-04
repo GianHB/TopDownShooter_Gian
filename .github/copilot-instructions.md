2735f2fb59288f3cc2ea39155b494743f5761d8f
GianHB <gianhuaman79@gmail.com>
2025-06-04 11:45:19 -0500

    Funcionalidad play

---
+++ .github/copilot-instructions.md
@@ -0,0 +1,4 @@
+<!-- UNITY CODE ASSIST INSTRUCTIONS START -->
+- Project name: TopDownShooter_Gian
+- Unity version: Unity 6000.0.32f1
+<!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Assets/Scripts/Scene/MenuScene.cs
+++ Assets/Scripts/Scene/MenuScene.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 using GameJolt.API;
 using UnityEngine.UI;
 using GameJolt.UI;
+using UnityEngine.SceneManagement;
 
 public class MenuScene : MonoBehaviour
 {
     [SerializeField] private Button trophiesButton;
+    [SerializeField] private Button playButton;
 
     private void Awake()
     {
@@ -19,5 +21,15 @@ public class MenuScene : MonoBehaviour
     private void Start()
     {
         Trophies.Unlock(268997);
+
+        if (playButton != null)
+        {
+            playButton.onClick.AddListener(OnPlayButton);
+        }     
+    }
+
+    public void OnPlayButton()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("GamePlayScene");
     }
 }
